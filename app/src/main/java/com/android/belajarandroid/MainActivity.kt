cca4bf7f2098db360e800b943ef99347d0728e34
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-03 21:41:06 +0700

    Menambahkan data binding

--- app/src/main/java/com/android/belajarandroid/MainActivity.kt
+++ app/src/main/java/com/android/belajarandroid/MainActivity.kt
@@ -3,30 +3,38 @@ package com.android.belajarandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.android.belajarandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
-        button.setOnClickListener {
-            val text: String? = inputText.text.toString()
-            when (text?.trim()) {
-                null -> {
-                    textView.text = "Hello, World!"
-                    Toast.makeText(this@MainActivity, "Hello, World!", Toast.LENGTH_SHORT)
-                        .show()
-                }
-                "" -> {
-                    textView.text = "Hello, World!"
-                    Toast.makeText(this@MainActivity, "Hello, World!", Toast.LENGTH_SHORT)
-                        .show()
-                }
-                else -> {
-                    textView.text = text
-                    Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT)
-                        .show()
+        dataBinding.apply {
+            button.setOnClickListener {
+                val text: String? = inputText.text.toString()
+                when (text?.trim()) {
+                    null -> {
+                        textView.text = "Hello, World!"
+                        Toast.makeText(this@MainActivity, "Hello, World!", Toast.LENGTH_SHORT)
+                            .show()
+                        gambar.setImageResource(resources.getIdentifier("naruto", "drawable", packageName))
+                    }
+                    "" -> {
+                        textView.text = "Tidak boleh kosong!"
+                        Toast.makeText(this@MainActivity, "Tidak boleh kosong!", Toast.LENGTH_SHORT)
+                            .show()
+                        gambar.setImageResource(resources.getIdentifier("naruto", "drawable", packageName))
+                    }
+                    else -> {
+                        textView.text = text
+                        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT)
+                            .show()
+                        gambar.setImageResource(resources.getIdentifier(text, "drawable", packageName))
+                    }
                 }
             }
         }
